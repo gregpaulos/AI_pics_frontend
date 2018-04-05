71513394bd3306b68e14ce290cb25a0b7113d488
gregory paulos <gregorypaulos@gregorys-MacBook-Air.local>
2018-04-05 10:45:10 -0600

    tweaking styles for responsiveness

--- src/components/Upload/Uploadphoto.js
+++ src/components/Upload/Uploadphoto.js
@@ -3,13 +3,9 @@ import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { showUserPhoto, addFile } from "../../actions";
 import GetAI from "./GetAI";
+import MediaQuery from "react-responsive";
 
-const Uploadphoto = ({
-  photo,
-  file,
-  showUserPhoto,
-  addFile
-}) => {
+const Uploadphoto = ({ photo, file, showUserPhoto, addFile }) => {
   const onChange = event => {
     let newPic = event.target.files["0"];
     let localurl = window.URL.createObjectURL(newPic);
@@ -25,7 +21,7 @@ const Uploadphoto = ({
     },
     fileinput: {
       borderRadius: "8px",
-      padding: "10px",
+      padding: "10px"
     },
     photo: {
       width: "400px",
@@ -37,20 +33,39 @@ const Uploadphoto = ({
     appBarLarge: { backgroundColor: "white" },
     titleLarge: {
       color: "rgb(100, 100, 100)"
-    }
+    },
+    noBorder: { padding: "0px", margin: "0px" }
   };
 
   return (
     <div>
       <form>
         <div>
+
+     <MediaQuery query="(min-device-width: 1224px)">
           <h3>Choose an image to upload (PNG, JPG)</h3>
+
+      </MediaQuery>
+
+      <MediaQuery query="(max-device-width: 1224px)">
+      <h3 style={styles.noBorder} >Choose an image to upload (PNG, JPG)</h3>
+
+      </MediaQuery>
+
+
+
+
           <p style={styles.disclaimer}>
             Please DO NOT put anything offensive up here. You know what I'm
             talking about.
           </p>
-          <input
-          style={styles.fileinput}
+         
+        </div>
+
+             <MediaQuery query="(min-device-width: 1224px)">
+             <div>
+             <input
+            style={styles.fileinput}
             type="file"
             id="image_uploads"
             name="image_uploads"
@@ -58,9 +73,28 @@ const Uploadphoto = ({
             multiple
             onChange={onChange}
           />
-        </div>
+
+             </div>
         <img src={photo} style={styles.photo} alt="" />
-        {file.length > 0 ? <GetAI /> : ""}
+             {file.length > 0 ? <GetAI /> : ""}
+
+      </MediaQuery>
+
+      <MediaQuery query="(max-device-width: 1224px)">
+      {file.length > 0 ? <GetAI /> :  <input
+            style={styles.fileinput}
+            type="file"
+            id="image_uploads"
+            name="image_uploads"
+            accept=".jpg, .jpeg, .png"
+            multiple
+            onChange={onChange}
+          />}
+        <img src={photo} style={styles.photo} alt="" />
+
+      </MediaQuery>
+
+        
       </form>
     </div>
   );
